e3e6c480fb57cabc680b376a0d94af9290fe5783
Alex Burkhart + Aaron Christy <pairing+ab+ac@edgecase.com>
2013-11-15 09:02:13 -0500

    Create a log directory when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ require 'rspec/given'
 # require all support files
 Dir["./spec/support/**/*.rb"].each { |f| require f }
 
+# generate log directory for revolution gem
+log_directory = Pathname.new(Netsweet.config.revolution_prep_log_path).dirname
+Dir.mkdir(log_directory) unless Dir.exists?(log_directory)
+
 RSpec.configure do |c|
   c.mock_with :rspec
 end
