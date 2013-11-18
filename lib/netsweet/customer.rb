f60ab3a0c844afe436eabfc2dd296358c0821904
Andrew Max <amax@forever.com>
2013-11-18 17:17:23 -0500

    removed error-worthy wording

--- lib/netsweet/customer.rb
+++ lib/netsweet/customer.rb
@@ -30,7 +30,8 @@ module Netsweet
       rvp_customer = NetSuite::Records::Customer.new(attrs)
       if rvp_customer.add
         Customer.new(rvp_customer)
-      else raise CustomerNotCreated.new("Customer could not be created. Customer: #{attrs}")
+      else raise CustomerNotCreated.new("Customer: \"#{attrs}\" could
+                                        not be created.")
       end
     end
 
